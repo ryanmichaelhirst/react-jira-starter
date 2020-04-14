d7999971a3da6163943f08e9d2464addf49dc4ab
RyanMichael Hirst <rmbh4211995@gmail.com>
2020-04-14 16:03:19 -0400

    Update starting point.

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import Col from "../components/Col";
+import { data, statusIcons } from "../data";
 
 const Homepage = () => {
-    return <div>We start here!</div>
+    const [items, setItems] = useState(data);
+
+    return (
+        <div className="row">
+            {["open", "in progress", "in review", "done"].map(status => {
+                return (
+                    <div key={status} className={"col-wrapper"}>
+                        <div className={"col-group"}>
+                            <h5 className={"col-header"}>{status.toUpperCase()}</h5>
+                            <p className={"col-count"}>{items.filter(i => i.status === status).length}</p>
+                        </div>
+                        <Col>
+                            {items
+                                .filter(i => i.status === status)
+                                .map(i => (
+                                    <div key={i.id}>Item! {i.content}</div>
+                                ))
+                            }
+                            <button onClick={e => onAddItem(status)}>
+                                Add ticket
+                            </button>
+                        </Col>
+                    </div>
+                )
+            })}
+        </div>  
+    );
 };
 
 export default Homepage;
